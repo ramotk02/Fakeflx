8e1561f3580228bd2e0d4d031a7a504d5c8d57e6
ramotk02 <omartaky.ot@gmail.com>
2024-02-03 22:14:20 +0100

    color up

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,8 +5,10 @@ import React from "react";
 
 const MovieCard = ({ name, image, rate, link }) => {
   return (
-    <div className="bg-[black] w-[250px] text-center rounded-xl overflow-hidden relative max-w-xs bg-cover bg-no-repeat">
+    <div className="bg-[#2D2D2D] w-[250px] text-center rounded-xl overflow-hidden relative max-w-xs bg-cover bg-no-repeat">
+              <a href={link} target="_blank">
       <img src={image} alt={`Poster for ${name}`} className="w-full max-w-xs transition duration-300 ease-in-out hover:scale-110" />
+      </a>
       <div className="py-2">
         <span className="text-2xl">{name}</span>
         <p className="text-lg">
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import img from "./assets/img2.png";
+import img from "./assets/img1.png";
 
 const Navbar = () => {
   return (
-    <section className="bg-[black] h-[100px] flex justify-between items-center px-[30px]">
+    <section className="bg-[#2D2D2D] h-[100px] flex justify-between items-center px-[30px]">
       <div>
         <a href="/">
           <img className="h-[100px]" src={img} alt="img" />
@@ -14,7 +14,7 @@ const Navbar = () => {
         <input
           type="search"
           placeholder="Search here!"
-          className="bg-[#242e2f] px-[15px] py-[10px] rounded-md w-[350px] text-white "
+          className="bg-[#171616] px-[15px] py-[10px] rounded-md w-[350px] text-white "
         />
       </div>
     </section>
