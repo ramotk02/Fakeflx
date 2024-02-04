ad4977621edd4b09cf77cefb8f1f28187e9d8627
ramotk02 <omartaky.ot@gmail.com>
2024-02-04 13:28:01 +0100

    add films

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,19 +1,18 @@
-// MovieCard.js
 import React from "react";
 
 
 
 const MovieCard = ({ name, image, rate, link }) => {
   return (
-    <div className="bg-[#2D2D2D] w-[250px] text-center rounded-xl overflow-hidden relative max-w-xs bg-cover bg-no-repeat">
+    <div className="bg-[#2D2D2D] w-[200px] text-center rounded-xl overflow-hidden relative max-w-xs bg-cover bg-no-repeat transition duration-800 ease-in-out hover:scale-110">
               <a href={link} target="_blank">
-      <img src={image} alt={`Poster for ${name}`} className="w-full max-w-xs transition duration-300 ease-in-out hover:scale-110" />
+      <img src={image} alt={`Poster for ${name}`} className="w-full max-w-xs " />
       </a>
-      <div className="py-2">
+      <div className="py-1">
         <span className="text-2xl">{name}</span>
         <p className="text-lg">
           Rated {rate} |{""}
-          <a href={link} className="text-yellow-300" target="_blank" rel="noopener noreferrer">
+          <a href={link} className="text-red-800" target="_blank" rel="noopener noreferrer">
             Link
           </a>
         </p>
--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -3,30 +3,39 @@ import React from 'react';
 import MovieCard from './MovieCard';
 import Jmj from './assets/jmj.png';
 import batman from './assets/batman.jpeg';
-import dictator from './assets/dictator.jpeg';
 import flash from './assets/flash.jpeg';
 import under from './assets/under.webp';
 import hw from './assets/highwaymen.jpeg'
 import cmiyc from './assets/aff2.webp'
 import lift from './assets/lift.webp'
 import mib from './assets/mib3.jpeg'
+import dictator2 from './assets/dictator2.jpeg'
+import fg from './assets/fg.jpeg'
+import spirit from './assets/spirit.jpeg'
+import split from './assets/split.webp'
+
+
 const MovieList = () => {
   const films = [
     { name: "Jumanji", image: Jmj, link: "https://www.netflix.com/watch/80192646?trackId=255824129&tctx=0%2C1%2C645c6ef6-405c-42cf-9850-11b0ffde8c52-318290276%2C645c6ef6-405c-42cf-9850-11b0ffde8c52-318290276%7C2%2Cunknown%2C%2C%2CtitlesResults%2C80192646%2CVideo%3A80192646%2CminiDpPlayButton", rate: 10 },
-    { name: "Batman", image: batman, link: "https://www.netflix.com/title/70079583", rate: 10 },
-    { name: "The Dictator", image: dictator, link: "https://www.netflix.com/watch/70228042?trackId=255824129&tctx=1%2C6%2C01266522-2872-46ba-b661-c00c0390bb43-194980132%2C01266522-2872-46ba-b661-c00c0390bb43-194980132%7C2%2Cunknown%2C%2C%2CtitlesResults%2C70228042%2CVideo%3A70228042%2CminiDpPlayButton", rate: 9 },
     { name: "The Flash", image: flash, link: "https://www.netflix.com/search?q=the%20fla&jbv=80027042", rate: 6 },
     { name: "6 Underground", image: under, link: "https://www.netflix.com/search?q=6%20under&jbv=81001887", rate: 10 },
     { name: "The Highwaymen", image: hw, link: "https://www.netflix.com/title/80200571", rate: 8.5 },
-    { name: "Catch if you can", image: cmiyc, link: "https://www.netflix.com/title/60024942", rate: 8 },
-    { name: "Lift", image: lift, link: "https://www.netflix.com/title/81446739", rate: 10 },
     { name: "Men in Black 3", image: mib, link: "https://www.netflix.com/title/70217910", rate: 10 },
+    { name: "Catch me if you can", image: cmiyc, link: "https://www.netflix.com/title/60024942", rate: 8 },
+    { name: "Batman", image: batman, link: "https://www.netflix.com/title/70079583", rate: 10 },
+    { name: "The Dictator", image: dictator2, link: "https://www.netflix.com/watch/70228042?trackId=255824129&tctx=1%2C6%2C01266522-2872-46ba-b661-c00c0390bb43-194980132%2C01266522-2872-46ba-b661-c00c0390bb43-194980132%7C2%2Cunknown%2C%2C%2CtitlesResults%2C70228042%2CVideo%3A70228042%2CminiDpPlayButton", rate: 9 },
+    { name: "Lift", image: lift, link: "https://www.netflix.com/title/81446739", rate: 10 },
+    { name: "Forrest Gump", image: fg, link: "https://www.netflix.com/ma-fr/title/60000724", rate: 10 },
+    { name: " Spirit", image: spirit, link: "https://www.netflix.com/ma-fr/title/60020810", rate: 10 },
+    { name: " Split", image: split, link: "https://www.netflix.com/ma-fr/title/80124506", rate: 10 },
 
+    
   ];
 
   return (
     <section>
-      <div className="flex gap-4 p-[60px] flex-wrap justify-center">
+      <div className="flex gap-10 p-[60px] flex-wrap justify-center">
         {films.map((film, index) => (
           <MovieCard
             key={index}
